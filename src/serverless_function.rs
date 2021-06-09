9446cf25fe4eeba98f1f5709531b3dd5e6c7d781
Sahil Garg <68533287+sahiledjx@users.noreply.github.com>
2021-06-09 17:44:50 +0530

    Update serverless_function.rs

--- src/serverless_function.rs
+++ src/serverless_function.rs
@@ -1,14 +1,10 @@
+use edjx::{EdjLogger, HttpRequest, HttpResponse, StatusCode};
 
-use edjlib::{EdjLogger, HttpRequest, HttpResponse, StatusCode};
-use std::str::FromStr;
-
-pub fn serverless(req: HttpRequest) -> HttpResponse {
+pub fn serverless(_req: HttpRequest) -> HttpResponse {
 
     EdjLogger::info("Inside example function");
 
-    let mut res = HttpResponse::from("Welcome to EDJX".to_string())
+    HttpResponse::from("Welcome to EDJX".to_string())
         .set_status(StatusCode::OK)
-        .set_header("Server".parse().unwrap(), "EDJX".parse().unwrap());   
-
-    return res;
+        .set_header("Server".parse().unwrap(), "EDJX".parse().unwrap())
 }
