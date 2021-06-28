4a2e5bbc246da0ef7f77d8d54e114e89e6dfc85b
Sahil Garg <68533287+sahiledjx@users.noreply.github.com>
2021-06-28 18:15:59 +0530

    Update serverless_function.rs

--- src/serverless_function.rs
+++ src/serverless_function.rs
@@ -1,8 +1,8 @@
-use edjx::{EdjLogger, HttpRequest, HttpResponse, StatusCode};
+use edjx::{info, HttpRequest, HttpResponse, StatusCode};
 
 pub fn serverless(_req: HttpRequest) -> HttpResponse {
 
-    EdjLogger::info("Inside example function");
+    info!("Inside example function");
 
     HttpResponse::from("Welcome to EDJX".to_string())
         .set_status(StatusCode::OK)
